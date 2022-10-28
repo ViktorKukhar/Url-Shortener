17b0f718ac28d500ad04a0011c9af83a7003aee4
Viktor Kukhar <kukharviktor@icloud.com>
2022-10-28 23:24:26 +0300

    upd 28.10

--- Gemfile
+++ Gemfile
@@ -26,9 +26,7 @@ gem "importmap-rails"
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
 gem "turbo-rails"
 
-gem 'clipboard-rails'
 
-gem 'jquery-turbolinks'
 
 # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
 gem "stimulus-rails"
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require clipboard
+
 import "@hotwired/turbo-rails"
 import "controllers"
 import * as bootstrap from "bootstrap"
-
-$(document).ready(function(){
-
-  var clipboard = new Clipboard('.clipboard-btn');
-  console.log(clipboard);
-
-});
